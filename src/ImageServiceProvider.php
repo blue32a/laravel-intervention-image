1f805d93871940f1efc4ccadb243c53fbb0c44be
blue32a <blue32a.jp@gmail.com>
2024-01-01 11:35:30 +0900

    feat: Add service provider

---
+++ src/ImageServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blue32a\Laravel\Intervention\Image;
+
+use Illuminate\Support\ServiceProvider;
+use Intervention\Image\ImageManager;
+
+class ImageServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->singleton(ImageManager::class, function () {
+            return new ImageManager(config('image.driver'));
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__.'/../config/image.php' => config_path('image.php'),
+        ]);
+    }
+}
